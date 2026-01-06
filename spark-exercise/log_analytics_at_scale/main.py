16b19a17c130717909f7717208e9bdc131b7fe05
anurodh <anurodhk82@gmail.com>
2026-01-06 19:01:53 +0530

    feat: avg response time per endpoint

--- spark-exercise/log_analytics_at_scale/main.py
+++ spark-exercise/log_analytics_at_scale/main.py
@@ -1,18 +1,23 @@
-import os
 from pyspark.sql import SparkSession, Window
-from pyspark.sql.functions import to_date, col, desc, asc, dense_rank
-from random_data_generator import (
-    random_timstamps,
-    generate_endpoint_and_resp_codes,
-    generate_ip,
-    generate_response_time,
-)
+from pyspark.sql.functions import to_date, col, desc, asc, dense_rank, avg
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType, DateType
 
 spark = SparkSession.builder.appName("log_analytics").getOrCreate()
+
+logs_schema = StructType(
+    [
+        StructField("timestamp", DateType(), False),
+        StructField("ip", StringType(), False),
+        StructField("endpoint", StringType(), False),
+        StructField("response_codes", IntegerType(), False),
+        StructField("response_time", IntegerType(), False),
+    ]
+)
+
+logs_df = spark.read.schema(logs_schema).csv("data/my_logs.csv", header=True)
 # create_append_logs(1000000)
 
 """ Top 10 most visited endpoints per day """
-logs_df = spark.read.csv("data/my_logs.csv", header=True)
 logs_with_date = logs_df.withColumn("date", to_date(col("timestamp")))
 # logs_with_date.show()
 
@@ -21,9 +26,14 @@ wind_spec = Window.partitionBy("date").orderBy(col("count").desc())
 resultant_df = logs_with_endpoint_count.withColumn(
     "rank", dense_rank().over(wind_spec)
 ).filter(col("rank") <= 10)
-resultant_df.show(n=resultant_df.count())
+resultant_df.show()
+# resultant_df.show(n=resultant_df.count())
 
 
 """ Average & P95 response time per endpoint """
+logs_df.groupBy("endpoint").agg(
+    avg(col("response_time")).alias("avg_response_time")
+).show()
+
 
 """ Detect IPs making >1000 requests/hour """
