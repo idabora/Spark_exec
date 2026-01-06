34dfaf87697f05948573bcf70d2d532b45ce0704
anurodh <anurodhk82@gmail.com>
2026-01-06 17:05:35 +0530

    feat: random generator functions

---
+++ spark-exercise/log_analytics_at_scale/random_data_generator.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+import random
+
+# print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+# timestamp, user_id, ip, endpoint, response_code, response_time
+
+
+def random_timstamps(lines):
+    timestamps = []
+    base_timestamp = datetime.now()
+
+    for i in range(lines):
+        incremental_value = random.randint(1, 60)
+        base_timestamp = base_timestamp + timedelta(seconds=incremental_value)
+        new_timestmap = base_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        timestamps.append(new_timestmap)
+
+    # print(timestamps)
+    return timestamps
+
+
+def generate_ip(lines):
+    ip_addresses = []
+
+    for i in range(lines):
+        byte_one = str(random.randint(101, 200))
+        byte_two = str(random.randint(101, 400))
+        byte_three = str(random.randint(0, 99))
+        byte_four = str(random.randint(0, 99))
+
+        ip_address = ".".join([byte_one, byte_two, byte_three, byte_four])
+        ip_addresses.append(ip_address)
+    # print(ip_addresses)
+    return ip_addresses
+
+
+def generate_endpoint_and_resp_codes(lines):
+    endpoints = []
+    modules = {
+        "/home": ["about", "services", "pricing", "documentation", "profile"],
+        "/profile": ["details", "update", "view"],
+        "/auth": ["login", "signin", "signout"],
+        "/post": ["create", "edit", "delete", "get"]
+    }
+
+    response_codes = []
+    response = {
+        "/home": [200, 400, 500, 401, 403],
+        "/profile": [200, 201, 204, 400, 500, 401, 403],
+        "/auth": [200, 400, 500, 403],
+        "/post": [200, 201, 400, 401, 500]
+    }
+
+    for i in range(lines):
+        key = random.choice(list(modules))
+        val = random.choice(modules[key])
+        endpoint = "/".join([key, val])
+        endpoints.append(endpoint)
+
+        resp = random.choice(response[key])
+        response_codes.append(resp)
+    # print(response_codes)
+    return endpoints, response_codes
+
+
+def generate_response_time(lines):
+    response_time = []
+    for i in range(lines):
+        response_time.append(random.randint(100, 500))
+    # print(response_time)
+    return response_time
+
+# random_timstamps(100)
+# generate_ip(100)
+# generate_endpoint_and_resp_codes(100)
+# generate_response_time(100)
